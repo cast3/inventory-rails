da9d5c054675ea0078596f933b6b316abe448a1b
alex <alenux1@gmail.com>
2023-06-08 22:16:07 -0400

    [spec] Validate field required and some constrain

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,8 +1,10 @@
 FactoryBot.define do
   factory :product do
-    nombre { "MyString" }
-    descripcion { "MyString" }
-    cantidad { 1 }
-    precio_compra { "9.99" }
+    nombre { Faker::Name.first_name }
+    referencia { Faker::Lorem.sentence(word_count: 1, supplemental: true) }
+    precio { Faker::Number.decimal(l_digits: 2) }
+   #category_id { association(:category) }
+    created_at { Time.now }
+    updated_at { Time.now }
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    subject.validate
+  end
+  context 'check attr product' do
+    it { is_expected.to be_instance_of(Product) }
+    it { is_expected.to have_attributes(nombre: nil, referencia: nil, precio: nil, image: nil,fecha_caducidad: nil, category_id: nil, tipo: 'Producto')}
+  end
+  # attributes are already so we need to do some steps
+  context 'validations' do
+    context 'Fields required' do
+      it '#nombre' do
+        expect(subject.errors.messages_for(:nombre).first).to include('no puede estar en blanco')
+      end
+      it '#referencia' do
+        expect(subject.errors.messages_for(:referencia).first).to include('no puede estar en blanco')
+      end
+      it '#precio' do
+        expect(subject.errors.messages_for(:precio).first).to include('no puede estar en blanco')
+      end
+    end
+    context 'Rules validation' do
+      let(:product) { build_stubbed(:product) }
+      it 'nombre max length ' do
+        product.validate
+        expect(product.nombre).to_not be_nil
+        expect(product.nombre).to have_attributes(size: (be < 50))
+        expect(subject.errors.messages_for(:nombre).first).to_not include('max length')
+      end
+      it '#referencia max' do
+        product.validate
+        expect(product.referencia).to_not be_nil
+        expect(product.referencia).to have_attributes(size: (be < 15))
+        expect(subject.errors.messages_for(:referencia).first).to_not include('max length')
+      end
+    end
+  end
 end
